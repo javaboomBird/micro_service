a698e1471548f7fe27cf61baf63ac384c69c4e0c
huangzh <huangzh@gzmoninet.com>
2021-03-05 17:24:06 +0800

    feature: 添加seata示例

---
+++ order-service/src/test/java/com/huangzh/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangzh.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
