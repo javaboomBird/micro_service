a60bba649df1f22270cda13cc0bdd81ce37c43c9
huangzh <huangzh@gzmoninet.com>
2021-03-22 15:04:24 +0800

    feat: demo1

--- order-service/src/main/java/com/huangzh/orderservice/message/Demo01Consumer.java
+++ order-service/src/main/java/com/huangzh/orderservice/message/Demo01Consumer.java
@@ -2,11 +2,15 @@ package com.huangzh.orderservice.message;
 
 
 import com.huangzh.orderservice.model.OrderTable;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 @RabbitListener(queues = Demo01Message.QUEUE)
@@ -20,7 +24,9 @@ public class Demo01Consumer {
     }
 
     @RabbitHandler
-    public void onMessage1(OrderTable message) {
-        logger.info("[自定义消息消费成功][线程编号:{} 消息内容：{}]", Thread.currentThread().getId(), message);
+    public void onMessage1(OrderTable order, Channel channel, Message message) throws IOException {
+        logger.info("[自定义消息消费成功][线程编号:{} 消息内容：{}]", Thread.currentThread().getId(), order);
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        logger.info("小富收到消息：{}", message.getMessageProperties().getDeliveryTag());
     }
 }
